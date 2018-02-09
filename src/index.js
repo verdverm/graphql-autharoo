6b0d80008e1a9f5e34cdd0a4a6c398109abed258
Tony Worm <verdverm@gmail.com>
2018-02-08 23:09:49 -0700

    export validateScope

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import authSwitch from './switch';
 import authBatching from './batching';
+import { validateScope } from './validators';
 
 export default {
+  validateScope,
   authSwitch,
   authBatching,
 };
